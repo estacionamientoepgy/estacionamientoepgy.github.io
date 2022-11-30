f71bdc445a2ef9b14d6957889a7aa4ddf91319ee
estacionamientoepgy <119455225+estacionamientoepgy@users.noreply.github.com>
2022-11-30 00:12:06 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+   apiKey: "AIzaSyBPsRBXrKIk7MiQy3nJ4Y_DP594VY12J18",
+  authDomain: "estacionamientoepg.firebaseapp.com",
+  projectId: "estacionamientoepg",
+  storageBucket: "estacionamientoepg.appspot.com",
+  messagingSenderId: "914456470409",
+  appId: "1:914456470409:web:6cbe8c227e92cb6d563758",
+  measurementId: "G-Z8EE7P9EEN"
 });
