d59aac2435a6ea712f62d4a9b28959e6a53e6a68
estacionamientoepgy <119455225+estacionamientoepgy@users.noreply.github.com>
2022-11-30 00:08:42 -0600

    Add files via upload

---
+++ js/CtrlCarroNuevo.js
@@ -0,0 +1,65 @@
+import {
+  getAuth,
+  getFirestore
+} from "../lib/fabrica.js";
+import {
+  getString,
+  muestraError
+} from "../lib/util.js";
+import {
+  muestraAlumnos
+} from "./navegacion.js";
+import {
+  tieneRol
+} from "./seguridad.js";
+
+const daoCarro =
+  getFirestore().
+    collection("Carro");
+/** @type {HTMLFormElement} */
+const forma = document["forma"];
+getAuth().onAuthStateChanged(
+  protege, muestraError);
+
+/** @param {import(
+    "../lib/tiposFire.js").User}
+    usuario */
+async function protege(usuario) {
+  if (tieneRol(usuario,
+    ["Administrador"])) {
+    forma.addEventListener(
+      "submit", guarda);
+  }
+}
+
+/** @param {Event} evt */
+async function guarda(evt) {
+  try {
+    evt.preventDefault();
+    const formData =
+      new FormData(forma);
+    const matricula = getString(
+        formData, "matricula").trim();  
+    const nombrep = getString(formData, "nombrep").trim();
+    const telefono = getString(formData, "telefono").trim();
+    const marca = getString(formData, "marca").trim();
+    const fecha = getString(formData, "fecha").trim();
+    /**
+     * @type {
+        import("./tipos.js").
+                Alumno} */
+    const modelo = {
+      matricula,
+      nombrep,
+      telefono,
+      marca,
+      fecha 
+    };
+    await daoCarro.
+      add(modelo);
+    muestraCarro();
+  } catch (e) {
+    muestraError(e);
+  }
+}
+
